121692a61e45bccb3eb23a1fb47e1be3f61a8c77
Aristotele <aristotele.songhori@gmail.com>
2022-07-15 11:55:17 +0200

    add Menu
    Ver 2 by Menu

--- app/src/main/java/com/aristotele/trivia2022/MainActivity.kt
+++ app/src/main/java/com/aristotele/trivia2022/MainActivity.kt
@@ -2,13 +2,43 @@ package com.aristotele.trivia2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.lifecycle.whenCreated
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-
         setContentView(R.layout.activity_main)
+
+
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+        // return super.onCreateOptionsMenu(menu)
+
+    }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId){
+            R.id.nav_menu_1 -> Log.i("sexyLog","item1")
+            R.id.nav_menu_2 -> Log.i("sexyLog","ite2")
+            R.id.nav_menu_3 -> Log.i("sexyLog","item3")
+        }
+        return super.onOptionsItemSelected(item)
+
+    }
+
+
+
+
 }
